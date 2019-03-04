872054f67eaa5f1f2f31aaf7d3ea3d9c578514fc
javadjafariv94 <javadjafariv94@gmail.com>
2019-03-04 15:08:08 +0330

    React.createElement into inside

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+//----
 const App = () => {
-    return React.createElement('h1',{className:'Title'},'Hello world');
+    return React.createElement('h1',{className:'Title'},'Hello world',React.createElement('div'));
 }
 
 ReactDOM.render(<App/>,document.querySelector("#root")); 
