351127dc2adec78df19e5f7a85b0c37c2ed41deb
javadjafariv94 <javadjafariv94@gmail.com>
2019-03-04 18:38:37 +0330

    css with out import

--- src/components/header.js
+++ src/components/header.js
@@ -3,20 +3,10 @@ import React, {Component} from 'react';
 
 class Header extends Component {
   render() {
-    const styles = {
-      header: {
-        background :'#03a9f4'
-      },
-      logo: {
-        color:'#fff',
-        fontFamily:'Anton',
-        textAlign:'center' 
-      }
-    }
-
+    
     return(
-      <header style={styles.header}>  
-        <div style={styles.logo}>  logo </div>
+      <header >  
+        <div  >  logo </div>
         <input type="Text"/>
       </header>
     )
