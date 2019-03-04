937ecdcd128b72cc5ed59a3be67759095f30b4e0
javadjafariv94 <javadjafariv94@gmail.com>
2019-03-04 16:34:18 +0330

    new Date

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-
+const getYear = () => {
+  const year = new Date();
+  return year.getFullYear();
+}
 
 const Header = () => {
   return(
-    <div> The date is : { Date.now() } </div>
+    <div> The date is { getYear() } </div>
   )
 } 
 export default Header;
