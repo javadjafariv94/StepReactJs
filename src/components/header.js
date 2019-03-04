ef68b017e2cdd79edf70b03c028b2c69c475a745
javadjafariv94 <javadjafariv94@gmail.com>
2019-03-04 17:43:04 +0330

    function then class component

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 
-const Header = () => {
-  return(
-    <div>  
-      <div> logo </div>
-      <input type="Text"/>
-    </div>
-  )
-} 
+class Header extends React.Component {
+  render() {
+    return(
+      <div>  
+        <div> logo </div>
+        <input type="Text"/>
+      </div>
+    )
+  } 
+}
 export default Header;
