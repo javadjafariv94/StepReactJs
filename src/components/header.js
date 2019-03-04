0a4168ee14641c44def78529ab5683e02505aca2
javadjafariv94 <javadjafariv94@gmail.com>
2019-03-04 17:38:55 +0330

    function then component

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,11 @@
 import React from 'react';
 
-const user = {
-    name : "javad",
-    lastname : "jagfari",
-    age :42
-}
 
 const Header = () => {
   return(
     <div>  
-      <div> {user.name} </div>
-      <div> {user.lastname}</div>
-      <div> {user.age} </div>
+      <div> logo </div>
+      <input type="Text"/>
     </div>
   )
 } 
