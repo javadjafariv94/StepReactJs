1a6e4107e4c93d823b4a42fea402ead27e967a89
javadjafariv94 <javadjafariv94@gmail.com>
2019-03-04 15:03:39 +0330

    React.createElement

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <h1> Hello World !!!</h1>
+    return React.createElement('h1',{className:'Title'},'Hello world');
 }
 
 ReactDOM.render(<App/>,document.querySelector("#root")); 
