bfc38ba0f62c6cecca1b7b1d2dabdc66af20fc61
javadjafariv94 <javadjafariv94@gmail.com>
2019-03-05 13:15:09 +0330

    ok

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
 import '../components/css/styles.css'
+ 
+function one(name){
+   console.log(name);
+ }
+
+ function two(name){
+   one(name);
+ }
+
+ two('javad');
+
 
 class Header extends Component {
   inputChangeHandler= (myevent)=>{
     console.log(myevent.target.value);
+    
   }
 
   render() {
@@ -11,7 +23,7 @@ class Header extends Component {
     return(
       <header >  
         <div className="logo">  logo </div>
-        <input type="Text" onChange={(e1)=> this.inputChangeHandler(e1)}/>
+        <input type="Text" onChange={(evn1)=> this.inputChangeHandler(evn1)}/>
       </header>
     )
   } 
