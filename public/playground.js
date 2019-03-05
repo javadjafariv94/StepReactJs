75a735e925df81afcf5a82e93e049a00772a1318
javadjafariv94 <javadjafariv94@gmail.com>
2019-03-05 14:41:35 +0330

    super

--- public/playground.js
+++ public/playground.js
@@ -1,14 +1,20 @@
 class Car {
   constructor(){
-     this.wheels = 4;
+     this.wheels = 5;
      this.type = 'Hatchback'
     }
 }    
 
 class Ford extends Car {
+    constructor() {
+        super();
+        this.windowsCar = 4;
+    }
  
 }
 //inherit
 const ford = new Ford();
 
 console.log(ford.wheels);
+
+console.log(ford.windowsCar);
