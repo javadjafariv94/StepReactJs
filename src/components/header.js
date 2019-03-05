3e933da71ae6d68936a4b553a26c5bbe9737f4be
javadjafariv94 <javadjafariv94@gmail.com>
2019-03-05 10:56:03 +0330

    onclick function

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import '../components/css/styles.css'
 
 class Header extends Component {
+  inputChangeHandler= ()=>{
+    console.log("I was clicked");
+  }
+
   render() {
       
     return(
       <header >  
         <div className="logo"
-          onClick={()=>console.log("I was clicked")}
+          onClick={this.inputChangeHandler}
         >  logo </div>
         <input type="Text"/>
       </header>
